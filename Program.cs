da2524d15ae1047cf47b4485d5d876f80b858ce6
Ozan <oznkucuk@gmail.com>
2023-02-27 11:58:49 +0300

    icice for dongusu
    içiçe for döngüsü kullanarak ekrana çarpım tablosu yazdırma

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace carpim_tablosu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // İçiçe for döngüsü kullanılarak çarpım tablosu oluşturma
+            CarpimTablosu();
+
+
+        }
+
+        private static void CarpimTablosu()
+        {
+                       Console.WriteLine("*-*-*- ÇARPIM TABLOSU -*-*-*");
+
+            for (int i = 1; i <= 10; i++)
+            {   
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine(i+"x"+j+"="+(i*j));
+                    
+                }
+            Console.WriteLine("*****************");
+            }
+        }
+    }
+}
